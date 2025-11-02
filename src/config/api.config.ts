3fc931b49821649e4eab6ed1e7f06098877b1d73
Steve Boussa <jrkira84@gmail.com>
2025-11-02 19:14:13 +0100

    fix api url

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,10 @@
+// export const API_CONFIG = {
+//   BASE_URL: process.env.REACT_APP_API_URL || 'http://192.168.1.149:8000/api/v1',
+//   TIMEOUT: 30000,
+// };
+
 export const API_CONFIG = {
-  BASE_URL: process.env.REACT_APP_API_URL || 'http://192.168.1.149:8000/api/v1',
+  BASE_URL: process.env.REACT_APP_API_URL || 'http://admin.cleanestuaire.com/api/v1',
   TIMEOUT: 30000,
 };
 
